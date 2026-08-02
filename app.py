098a1e4744716113e764f447e42d3bab822f3a70
Sajuuk <gordon2655@hotmail.co.uk>
2026-08-02 14:22:23 +0100

    another typo fix (wrong variable)

--- app.py
+++ app.py
@@ -90,10 +90,10 @@ def latest_run(owner, game, cat, player):
 	# to be processed differently.
 	if isinstance(result, dict):
 		joined = format_multirun_output(result)
-		return f"{player.capitalize()} most recent verified runs in {game.upper()} {category_map[cat]['clean']}: {joined}"
+		return f"{player.capitalize()} most recent verified runs in {game.upper()} {config.CATEGORY_MAPPING[cat]['clean']}: {joined}"
 
 	# Just a regular run, so print the standard text supporting the latest run.
-	return f"{player.capitalize()} most recent verified run in {game.upper()} {category_map[cat]['clean']} is {result.time} (#{result.place}): {result.link}"
+	return f"{player.capitalize()} most recent verified run in {game.upper()} {config.CATEGORY_MAPPING[cat]['clean']} is {result.time} (#{result.place}): {result.link}"
 
 
 # Find a PB for the given player and category.
@@ -134,7 +134,7 @@ def personal_best(owner, game, cat, player):
 
 	# Print the standard string to represent this PB.
 	pb = result[0]
-	return f"{player.capitalize()} has a PB of {pb.time} (#{pb.place}) in {game.upper()} {category_map[cat]['clean']}: {pb.link}"
+	return f"{player.capitalize()} has a PB of {pb.time} (#{pb.place}) in {game.upper()} {config.CATEGORY_MAPPING[cat]['clean']}: {pb.link}"
 
 
 # Provide help and support to users calling the routes.
