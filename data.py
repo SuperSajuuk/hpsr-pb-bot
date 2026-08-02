f01358ac4ff92a69da76291a6cfc1c18464a68e2
Sajuuk <gordon2655@hotmail.co.uk>
2026-08-02 15:03:23 +0100

    var typo

--- data.py
+++ data.py
@@ -155,7 +155,7 @@ class SRDCRuns:
 			game=str(run_obj.game),
 			category=str(run_obj.category),
 			time=time,
-			emulator=run["system"]["emulated"],
+			emulator=run_ob["system"]["emulated"],
 			place=None,  # run search does not include leaderboard place
 			link=run_obj.weblink,
 		)
